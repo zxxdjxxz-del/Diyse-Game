c56a3ee2fa24aaa16fe9261d3ff654373b6f2988
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 11:58:24 -0500

    Expose First Command Warden owner loader

--- tools/diysim/encounters/story_bosses/first_command_warden/__init__.py
+++ tools/diysim/encounters/story_bosses/first_command_warden/__init__.py
@@ -1,5 +1,20 @@
 """First Command Warden repo-backed encounter support."""
 
+from .repo_loader import (
+    CommandSealRule,
+    FirstCommandWardenRepoData,
+    MajorRulingRule,
+    StateBProtectionRule,
+    load_first_command_warden_repo_data,
+)
 from .snapshots import WardenPartySnapshot, load_first_command_warden_party_snapshot
 
-__all__ = ["WardenPartySnapshot", "load_first_command_warden_party_snapshot"]
+__all__ = [
+    "CommandSealRule",
+    "FirstCommandWardenRepoData",
+    "MajorRulingRule",
+    "StateBProtectionRule",
+    "WardenPartySnapshot",
+    "load_first_command_warden_party_snapshot",
+    "load_first_command_warden_repo_data",
+]
