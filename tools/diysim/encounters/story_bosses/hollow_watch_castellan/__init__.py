2db049ffc0b97fe6b9f45b09a266a98646dbebcb
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 08:29:47 -0500

    Export Hollow Watch party snapshot helpers

--- tools/diysim/encounters/story_bosses/hollow_watch_castellan/__init__.py
+++ tools/diysim/encounters/story_bosses/hollow_watch_castellan/__init__.py
@@ -12,6 +12,7 @@ from .runtime import (
     run_hollow_watch_smart,
     simulate_hollow_watch_smart,
 )
+from .snapshots import load_hollow_watch_party_snapshot, with_hollow_watch_party_snapshot
 
 __all__ = [
     "HollowWatchOutcome",
@@ -19,7 +20,9 @@ __all__ = [
     "HollowWatchSummary",
     "SmartPolicyConfig",
     "collect_hollow_watch_source_gaps",
+    "load_hollow_watch_party_snapshot",
     "load_hollow_watch_repo_data",
     "run_hollow_watch_smart",
     "simulate_hollow_watch_smart",
+    "with_hollow_watch_party_snapshot",
 ]
