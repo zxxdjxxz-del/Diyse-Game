ef115c2d54804af72d433cbb9b14473000e7ecaf
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 11:48:38 -0500

    Export working balance overlays

--- tools/diysim/overlays/__init__.py
+++ tools/diysim/overlays/__init__.py
@@ -1,5 +1,20 @@
 """Non-destructive balance overlays and sensitivity tools."""
 
+from .balance import (
+    BalanceOverlay,
+    apply_enemy_balance_overlay,
+    scale_direct_damage_actions,
+    scale_direct_damage_power,
+    scale_effective_core_stat_level,
+)
 from .sweeps import scale_enemy_side, sweep_enemy_stats
 
-__all__ = ["scale_enemy_side", "sweep_enemy_stats"]
+__all__ = [
+    "BalanceOverlay",
+    "apply_enemy_balance_overlay",
+    "scale_direct_damage_actions",
+    "scale_direct_damage_power",
+    "scale_effective_core_stat_level",
+    "scale_enemy_side",
+    "sweep_enemy_stats",
+]
