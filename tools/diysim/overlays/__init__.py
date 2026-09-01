605de6baac01b9b68a53635da665b554bc5bcd10
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 17:50:51 -0500

    Export split stat overlay helper

--- tools/diysim/overlays/__init__.py
+++ tools/diysim/overlays/__init__.py
@@ -6,6 +6,7 @@ from .balance import (
     scale_direct_damage_actions,
     scale_direct_damage_power,
     scale_effective_core_stat_level,
+    scale_selected_core_stat_level,
 )
 from .sweeps import scale_enemy_side, sweep_enemy_stats
 
@@ -15,6 +16,7 @@ __all__ = [
     "scale_direct_damage_actions",
     "scale_direct_damage_power",
     "scale_effective_core_stat_level",
+    "scale_selected_core_stat_level",
     "scale_enemy_side",
     "sweep_enemy_stats",
 ]
