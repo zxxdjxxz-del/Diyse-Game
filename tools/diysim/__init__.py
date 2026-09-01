5adedcce025d14db762668e6d3f8fca8f80410f4
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-08-31 22:13:49 -0500

    Expose progression planner API

--- tools/diysim/__init__.py
+++ tools/diysim/__init__.py
@@ -17,21 +17,33 @@ from .core import (
     simulate,
     sweep_enemy_stats,
 )
+from .progression import (
+    ProgressionProjection,
+    ProgressionSegment,
+    exp_at_level_progress,
+    project_progression,
+    required_average_exp_per_encounter,
+)
 
 __all__ = [
     "ActionProfile",
     "BattleScenario",
     "CLASS_MULTIPLIERS",
     "CombatantTemplate",
+    "ProgressionProjection",
+    "ProgressionSegment",
     "SimulationSummary",
     "Stats",
     "adjusted_hit_chance",
     "cumulative_exp",
     "direct_damage",
+    "exp_at_level_progress",
     "exp_to_next_level",
     "level_from_exp",
     "natural_stats",
     "neutral_natural_stats",
+    "project_progression",
+    "required_average_exp_per_encounter",
     "simulate",
     "sweep_enemy_stats",
 ]
