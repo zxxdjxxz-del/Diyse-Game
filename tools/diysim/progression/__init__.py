5636153d55a92274c10386606c389f92d49adcb1
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-08-31 23:22:15 -0500

    Expose sectioned stats and EXP progression APIs

--- tools/diysim/progression/__init__.py
+++ tools/diysim/progression/__init__.py
@@ -1,5 +1,6 @@
 """Sectioned progression tools for the Diyse balance simulator."""
 
+from .player_exp import cumulative_exp, exp_to_next_level, level_from_exp, level_up_cost, round_nearest_100
 from .routes import (
     ProgressionCheckpoint,
     ProgressionProjection,
@@ -8,12 +9,24 @@ from .routes import (
     project_progression,
     required_average_exp_per_encounter,
 )
+from .stats import CLASS_MULTIPLIERS, LEVEL_CAP, STAT_KEYS, Stats, natural_stats, neutral_natural_stats
 
 __all__ = [
+    "CLASS_MULTIPLIERS",
+    "LEVEL_CAP",
     "ProgressionCheckpoint",
     "ProgressionProjection",
     "ProgressionSegment",
+    "STAT_KEYS",
+    "Stats",
+    "cumulative_exp",
     "exp_at_level_progress",
+    "exp_to_next_level",
+    "level_from_exp",
+    "level_up_cost",
+    "natural_stats",
+    "neutral_natural_stats",
     "project_progression",
     "required_average_exp_per_encounter",
+    "round_nearest_100",
 ]
