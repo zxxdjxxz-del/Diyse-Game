d97f4e9de48f48c47941b042dd559b34b50d7f7d
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 11:56:12 -0500

    Expose Warden party snapshots

---
+++ tools/diysim/encounters/story_bosses/first_command_warden/__init__.py
@@ -0,0 +1,5 @@
+"""First Command Warden repo-backed encounter support."""
+
+from .snapshots import WardenPartySnapshot, load_first_command_warden_party_snapshot
+
+__all__ = ["WardenPartySnapshot", "load_first_command_warden_party_snapshot"]
