d2348f2ee1fc61e108641c971d106a31c87a4ebd
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-01 22:26:48 -0500

    Export progression loadout audit API

--- tools/diysim/progression/__init__.py
+++ tools/diysim/progression/__init__.py
@@ -1,5 +1,18 @@
 """Sectioned progression tools for the Diyse balance simulator."""
 
+from .audit import (
+    CharacterLoadoutAudit,
+    audit_campaign,
+    audit_campaign_checkpoint,
+    audit_character_checkpoint,
+)
+from .loadouts import (
+    ProgressionAssumption,
+    ProgressionSourceGap,
+    ResolvedLoadout,
+    resolve_campaign_loadouts,
+    resolve_loadout,
+)
 from .player_exp import cumulative_exp, exp_to_next_level, level_from_exp, level_up_cost, round_nearest_100
 from .routes import (
     ProgressionCheckpoint,
@@ -12,11 +25,18 @@ from .routes import (
 from .stats import STAT_KEYS, Stats, class_multipliers, level_cap, natural_stats, neutral_natural_stats
 
 __all__ = [
+    "CharacterLoadoutAudit",
+    "ProgressionAssumption",
     "ProgressionCheckpoint",
     "ProgressionProjection",
     "ProgressionSegment",
+    "ProgressionSourceGap",
+    "ResolvedLoadout",
     "STAT_KEYS",
     "Stats",
+    "audit_campaign",
+    "audit_campaign_checkpoint",
+    "audit_character_checkpoint",
     "class_multipliers",
     "cumulative_exp",
     "exp_at_level_progress",
@@ -28,5 +48,7 @@ __all__ = [
     "neutral_natural_stats",
     "project_progression",
     "required_average_exp_per_encounter",
+    "resolve_campaign_loadouts",
+    "resolve_loadout",
     "round_nearest_100",
 ]
