47cfa997a5d31d7d3d19590797443ba70d419077
zxxdjxxz-del <zxxdjxxz@gmail.com>
2026-09-22 18:50:29 -0500

    Raise Chapter 1 absolute enemy cap for Southern Briar

--- game/exploration/encounter_balance.gd
+++ game/exploration/encounter_balance.gd
@@ -11,7 +11,7 @@ const CHAPTER_PROFILES := {
 		"expected_ordinary_exp_pool": 1000,
 		"typical_min": 2,
 		"typical_max": 3,
-		"max_enemies": 4,
+		"max_enemies": 5,
 	},
 	2: {
 		"expected_encounters": 19,
